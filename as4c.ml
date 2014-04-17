2e0886fdac042ad7c2e6bc941e13869f9c2c7f89
Krilnon <krilnon@gmail.com>
2014-04-16 22:27:45 -0700

    Echo input file.

--- as4c.ml
+++ as4c.ml
@@ -1,1 +1,18 @@
-print_endline "AS4"
+open Printf
+
+let () =
+    let argNum = Array.length Sys.argv in
+    if argNum = 2 then begin (* only accepts 1 arg, the filename to compile *)
+        let file = Sys.argv.(1) in
+        printf "input file is %s\n" file;
+        let reader = open_in file in
+        try
+            let line = input_line reader in
+            print_endline line;
+            flush stdout;
+            close_in reader
+
+        with e ->
+            close_in_noerr reader;
+            raise e
+    end else print_endline "usage: as4c [filename]"
